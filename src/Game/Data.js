8ef519067e3e7d8e60324e51a4affcfefb807fa8
Aiswarya-98 <aiswaryasample789@gmail.com>
2024-01-23 22:46:41 +0530

    first commit

---
+++ src/Game/Data.js
@@ -0,0 +1,87 @@
+const Data = [ 
+  { 
+      id: 1, 
+      name: "apple", 
+      img:  
+"https://th.bing.com/th/id/OIP.zIks3VS0forQYrPKecaJ8QHaFj?w=226&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 2, 
+      name: "orange", 
+      img:  
+"https://th.bing.com/th/id/OIP.FRitdsgiPwTF3qBLbvoVzQHaE8?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 3, 
+      name: "cherry", 
+      img:  
+"https://th.bing.com/th/id/OIP.53G4z6FbbVWwyGZbOwHZlwHaE6?w=237&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 4, 
+      name: "watermelon", 
+      img:  
+"https://th.bing.com/th/id/OIP.KghvU4nz3oHe8LNfKEi0PwHaFS?w=224&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 5, 
+      name: "grapes", 
+      img:  
+"https://th.bing.com/th/id/OIP.YHfRye3QE1OATII-CRCQCAHaF2?w=239&h=189&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 6, 
+      name: "kiwi", 
+      img:  
+"https://th.bing.com/th/id/OIP.nYqXsw4Dyp38kIcW413mogHaE8?w=250&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 7, 
+      name: "apple", 
+      img:  
+"https://th.bing.com/th/id/OIP.zIks3VS0forQYrPKecaJ8QHaFj?w=226&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 8, 
+      name: "orange", 
+      img:  
+"https://th.bing.com/th/id/OIP.FRitdsgiPwTF3qBLbvoVzQHaE8?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 9, 
+      name: "cherry", 
+      img:  
+"https://th.bing.com/th/id/OIP.53G4z6FbbVWwyGZbOwHZlwHaE6?w=237&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 10, 
+      name: "watermelon", 
+      img:  
+"https://th.bing.com/th/id/OIP.KghvU4nz3oHe8LNfKEi0PwHaFS?w=224&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 11, 
+      name: "grapes", 
+      img:  
+"https://th.bing.com/th/id/OIP.YHfRye3QE1OATII-CRCQCAHaF2?w=239&h=189&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+  { 
+      id: 12, 
+      name: "kiwi", 
+      img:  
+"https://th.bing.com/th/id/OIP.nYqXsw4Dyp38kIcW413mogHaE8?w=250&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+      matched: false, 
+  }, 
+]; 
+export default Data;
